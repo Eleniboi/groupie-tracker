d5217d3eedec7967cb64ec7f948e9a692381f448
SM Eleni <omafuoloche4@gmail.com>
2026-07-11 22:02:48 +0100

    Add Artist struct with concert details

--- main.go
+++ main.go
@@ -11,3 +11,13 @@ type Concert struct{
 	Location string
 	Dates []string
 }
+
+type Artist struct{
+
+	ID int	`json:id`
+	Name string  `json:name`
+	Image string
+	CreationDate int
+	FirstAlbum string
+	Concerts []string
+}
