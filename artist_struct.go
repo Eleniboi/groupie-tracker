bd6724340c8b8e89e3813776db5d613f41a4b790
somafu <omafuoloche4@gmail.com>
2026-07-13 17:07:43 +0100

    file structure

---
+++ artist_struct.go
@@ -0,0 +1,22 @@
+package main
+
+type Concert struct {
+	Location string
+	Dates    []string
+}
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Members      []string `json:"members"`
+	Concerts     []Concert
+}
+
+type Relation struct{
+
+	ID int `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
