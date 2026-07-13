044b651be49319961a059a1cbf352ccc10e79625
somafu <omafuoloche4@gmail.com>
2026-07-13 20:59:05 +0100

    handler

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+
+import(
+	"net/http"
+	"fmt"
+)
+
+func main(){
+
+
+	http.HandleFunc("/", homeHandler)
+	fmt.Println("server is live on http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil{
+		fmt.Println(err)
+		return 
+	}
+}
