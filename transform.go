e1de4493267bcb64fe80075aa1481def820942de
somafu <omafuoloche4@gmail.com>
2026-07-15 05:53:56 +0100

    bug

--- transform.go
+++ transform.go
@@ -14,3 +14,30 @@ func BuildConcerts(dl map[string][]string) []Concert {
 	}
 	return Concertslice
 }
+
+func MergeArtistsWithRelations(artists []Artist, relations []Relation) []Artist {
+
+	RelationByID := map[int]Relation{}
+
+	for _, relation := range relations {
+
+		RelationByID[relation.ID] = relation
+	}
+
+	sliceArtist := []Artist{}
+
+	for _, artist := range artists {
+
+		NewArtist := Artist{}
+
+		matchedRelation := RelationByID[artist.ID]
+
+		mapper := matchedRelation.DatesLocations
+		Concertslice := BuildConcerts(mapper)
+
+		NewArtist.Concerts = Concertslice
+
+		sliceArtist = append(artist.Concerts, New)
+	}
+	return sliceArtist
+}
